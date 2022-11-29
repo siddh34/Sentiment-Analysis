b4c84b9c87bf536042cfc56a83cf1028aea3c405
siddh34 <ssiddharthsutar@gmail.com>
2022-11-30 00:40:22 +0530

    "Twitter Added analysed"

---
+++ twitterAPI.py
@@ -0,0 +1,45 @@
+import tweepy
+import pandas as pd
+import configparser
+
+
+# read configuration
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+API_Key = config['Twitter']['API_Key']
+API_Secret = config['Twitter']['API_Key_Secret']
+acessToken = config['Twitter']['access_token']
+acessSecretToken = config['Twitter']['access_secret_token']
+
+# print(API_Key, API_Secret, acessToken, acessToken)
+
+auth = tweepy.OAuth1UserHandler(API_Key,API_Secret)
+
+auth.set_access_token(acessToken,acessSecretToken)
+
+api = tweepy.API(auth)
+
+numberOfTweets = 100
+
+cursor = tweepy.Cursor(api.user_timeline,id='@CNN',tweet_mode="extended").items(numberOfTweets)
+
+tweetText = []
+tweetLikes = []
+time = []
+
+# printing data
+for i in cursor:
+    tweetText.append(i.full_text)
+    tweetLikes.append(i.favorite_count)
+    time.append(i.created_at)
+
+df = pd.DataFrame({'Tweet Text': tweetText,'Likes': tweetLikes,'Time': time})
+
+df.to_csv('CNN.csv')
+
+# part 2
+
+# api search for covid 
+# cursor = tweepy.Cursor(api.search,q="covid",tweet_mode="extended").items(1)
